7c312afaf3b91f1555d0bf74fba4c763abfaab1c
Huy <drac0123451@gmail.com>
2022-09-13 14:06:27 -0700

    add SQL query to server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -5,6 +6,23 @@ const app = express();
 // express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: 'nguyen1234',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query('select * from candidates', (err, rows) => {
+    console.log(rows);
+});
 
 // default response for any other request (not found)
 app.use((req, res) => {
