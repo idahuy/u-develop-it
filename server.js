3c1acbab4659913b8a7ccd541bb8924afe4cdfd5
Huy <drac0123451@gmail.com>
2022-09-13 14:43:08 -0700

    added sql queries

--- server.js
+++ server.js
@@ -24,6 +24,34 @@ db.query('select * from candidates', (err, rows) => {
     console.log(rows);
 });
 
+// get a single candidate
+db.query('select * from candidates where id =1', (err, row) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(row);
+});
+
+// Delete a candidate
+db.query('delete from candidates where id = ?', 1, (err, result) => {
+    if(err) {
+        console.log(err);
+    }
+    console.log(result);
+});
+
+// create a candidate
+const sql = `insert into candidates (id, first_name, last_name, industry_connected)
+                values (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
+    if(err) {
+        console.log(err);
+    }
+    console.log(result);
+});
+
 // default response for any other request (not found)
 app.use((req, res) => {
     res.status(404).end();
