adc698e390d78ca8a42a2835c778b5337f7b336a
Huy <drac0123451@gmail.com>
2022-09-15 11:41:26 -0700

    create table for parties and inserted data

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,10 @@ CREATE TABLE candidates (
   last_name VARCHAR(30) NOT NULL,
   industry_connected BOOLEAN NOT NULL
 );
+
+create table parties (
+  id integer auto_increment primary key,
+  name varchar(50) not null,
+  description text
+);
+
