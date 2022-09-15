337dd07dbc3f308815b5485a3f3c11c9b6fe1326
Huy <drac0123451@gmail.com>
2022-09-15 12:54:18 -0700

    updates sql files with voters

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,6 @@
 drop table if exists candidates;
 drop table if exists parties;
+drop table if exists voters;
 
 create table parties (
   id integer auto_increment primary key,
@@ -14,4 +15,12 @@ CREATE TABLE candidates (
   party_id integer,
   industry_connected BOOLEAN NOT NULL,
   constraint fk_party foreign key (party_id) references parties(id) on delete set null
+);
+
+CREATE TABLE voters (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
